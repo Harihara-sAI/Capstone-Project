1b7152b9b31d15a44954a39d0b408576e7503f07
Harihara-sAI <hahasamo1008@gmail.com>
2024-01-08 10:22:35 +0530

    Changes 8 Jan 2024 - 2
    Imported necessary libraries

---
+++ Microstructures.py
@@ -0,0 +1,17 @@
+# Imports 
+
+import os
+import cv2
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import MaxPooling2D, BatchNormalization
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.layers import Conv2D,MaxPool2D
+from keras.utils import normalize, to_categorical
+from keras import layers
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
