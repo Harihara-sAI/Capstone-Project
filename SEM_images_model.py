23a9fc20a7008c824e5bd655d33dd6b391c5d30c
Harihara-sAI <hahasamo1008@gmail.com>
2024-01-11 00:04:10 +0530

    Working model first phase
    The model now gives a decent accuracy of 0.88 in both training and validation while also having losses around 0.2

---
+++ SEM_images_model.py
@@ -0,0 +1,120 @@
+# Imports 
+
+import os
+import cv2
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+from tensorflow import keras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import MaxPooling2D, BatchNormalization
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.layers import Conv2D,MaxPool2D
+from keras.utils import normalize, to_categorical
+from keras import layers
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+
+
+plt.style.use('classic')
+
+# Initializations
+
+data_dir = "C:/Users/hahas/Downloads/Capstone Microstructure Data/"
+SIZE = 200
+batch_size = 32
+num_classes=3
+
+# Modifying images for easier working and labelling properly 
+
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=0,
+  image_size=(SIZE, SIZE),
+  batch_size=batch_size)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=0,
+  image_size=(SIZE, SIZE),
+  batch_size=batch_size)
+
+# Defining Data Augementation to combat overfitting
+
+data_augmentation = keras.Sequential(
+  [
+    layers.RandomFlip("horizontal",
+                      input_shape=(SIZE,
+                                  SIZE,
+                                  3)),
+    layers.RandomRotation(0.1),
+    layers.RandomZoom(0.1),
+  ]
+)
+
+
+# Defining model
+
+model = Sequential([
+  data_augmentation,
+  layers.Rescaling(1./255),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Dropout(0.2),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes, name="outputs")
+])
+
+
+# Compiling the model
+
+optimizer = keras.optimizers.Adam(lr=0.0001)
+model.compile(optimizer=optimizer,
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+
+model.summary()
+epochs=100
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+
+
+# Visualizing various parameters
+
+plt.figure(figsize=(15, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range,acc, label='Training Accuracy')
+plt.plot(epochs_range,val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range,loss, label='Training Loss')
+plt.plot(epochs_range,val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
