c02d148e61622e8c80f0697e8b0563d376f27268
Harihara-sAI <hahasamo1008@gmail.com>
2024-03-31 18:25:03 +0530

    updates

--- training_model.py
+++ training_model.py
@@ -73,5 +73,29 @@ history = model.fit(
   validation_data=val_ds,
   epochs=epochs
 )
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
 
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+#%%
 tf.keras.models.save_model(model,'my_model2.hdf5')
+
+plt.figure(figsize=(15, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range,acc, label='Training Accuracy')
+plt.plot(epochs_range,val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+plt.ylim(0,1)
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range,loss, label='Training Loss')
+plt.plot(epochs_range,val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+
+plt.savefig("results_2.png")
